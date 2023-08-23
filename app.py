f75eae4dfc1ea89d2b8b5fee5e9abbb17d50fa08
Kris Salmi <kristiina.k.salmi@gmail.com>
2023-08-23 22:28:48 +0300

    flask app, DigitTrainer class and model file

---
+++ app.py
@@ -0,0 +1,133 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import threading
+import redis
+import json
+import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
+from digit_trainer import DigitModelTrainer
+
+FRONTEND_URL = "http://localhost:3000"
+MODEL_FILE = "ai_digits_model.h5"
+MODEL_SCRIPT = "ai_digits.py"
+CUR_FOLDER = "."
+MODEL_CREATION_STATUS = ["not_started", "in_progress", "completed", "interrupted"]
+train_model_lock = threading.Lock() #global variable to lock the thread that is training the model, so multiple similar processes won't run at the same time
+stop_training_event = threading.Event() # Event that will signal the training process to stop
+global_model = None
+
+#Connect to an instance of Redis
+redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_conn.set('training_progress', '{}')
+redis_conn.set('model_status', MODEL_CREATION_STATUS[0])
+redis_conn.set('model_accuracy', 0)
+
+
+app = Flask(__name__)
+CORS(app, origins=[FRONTEND_URL])
+
+# Initialize the model trainer
+trainer = DigitModelTrainer()
+# Load model
+trainer.load_model()
+
+@app.route("/")
+def home():
+    return jsonify({"status": "healty"})
+
+@app.route('/api/drawing/', methods=['POST'])
+def send_drawing():
+    if not os.path.isfile(os.path.join(CUR_FOLDER, MODEL_FILE)):
+        return jsonify({'message': 'Model file does not exist. Please create the model first.'}), 404
+    try:
+        drawing = request.json['drawing']
+        decoded_drawing = decode_drawing(drawing)
+        preprocessed_drawing = preprocess_drawing(decoded_drawing)
+        prediction, confidence = trainer.predict_digit(preprocessed_drawing)
+        if prediction is None or confidence is None:
+            return jsonify({'error': "Model does not exist"}), 503
+        return jsonify({'prediction': prediction, 'confidence': float(confidence)})
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'An error occurred'}), 500
+    
+
+def decode_drawing(image_data):
+    # Remove preceding 'data:image/png;base64,', decode and convert to PIL image obj
+    base64_data = image_data.split(',')[1]
+    byte_data = base64.b64decode(base64_data)
+    drawing = Image.open(BytesIO(byte_data))
+    return drawing
+
+ # Resize and convert to grayscale, convert to numpy array, normalize
+def preprocess_drawing(drawing):
+    drawing = drawing.resize((28, 28)).convert('L') 
+    image_arr = np.array(drawing) / 255.0  # Convert to numpy array and normalize
+    return image_arr
+    
+@app.route('/api/retrain_model/', methods=['GET'])
+def retrain_model():
+    global train_model_lock, stop_training_event
+
+    # Attempt to acquire lock without blocking. If another thread already has
+    # the lock (someone else is training the model at the same time), this will return False immediately, avoiding a race condition
+    if not train_model_lock.acquire(blocking=False):
+        return jsonify({'message': 'Training is already in progress. Please try again later.'}), 409  # 409 Conflict
+    
+    stop_training_event.clear()
+    # Start the model creation in a separate thread, so response can be sent immediately
+    thread = threading.Thread(target=create_model_file)
+    thread.start()
+    redis_conn.set('model_status', MODEL_CREATION_STATUS[1])
+    return jsonify({'message': 'Model creation started'}), 202 #202 Accepted status code is used to indicate that a request \
+                                                    #has been accepted for processing, but the processing has not yet been completed
+
+@app.route('/api/stop_training/', methods=['GET'])
+def stop_training():
+    print("Gonna stop training...")
+    global stop_training_event
+    stop_training_event.set()
+    return jsonify({'message': 'Stop signal sent.'})
+    
+@app.route('/api/check_model_status/', methods=['GET'])
+def check_model_status():
+    status = redis_conn.get('model_status').decode("utf-8")
+    return jsonify({'model_status': status})
+
+@app.route('/api/training_progress/', methods=['GET'])
+def get_training_progress():
+    tp_value = redis_conn.get('training_progress')
+    if tp_value:
+        training_progress = json.loads(tp_value.decode("utf-8"))
+        return jsonify(training_progress)
+    else:
+        return jsonify({'message': 'No training progress available'})
+
+@app.route('/api/model_accuracy/', methods=['GET'])
+def get_model_accuracy():
+    try:
+        model_accuracy_bytes = redis_conn.get('model_accuracy')
+        model_accuracy = float(model_accuracy_bytes.decode("utf-8"))
+    except:
+        model_accuracy = None
+    return jsonify({'accuracy': model_accuracy})
+    
+def create_model_file():
+    try:
+        _, accuracy = trainer.build_model(stop_training_event)
+        redis_conn.set('model_accuracy', accuracy)
+        if stop_training_event.is_set():
+            redis_conn.set('model_status', MODEL_CREATION_STATUS[3])
+        else:
+            redis_conn.set('model_status', MODEL_CREATION_STATUS[2])
+            # Load new model into memory after a successfull creation
+            trainer.load_model()
+    finally:
+        train_model_lock.release()
+
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ digit_trainer.py
@@ -0,0 +1,139 @@
+from tensorflow import keras
+import redis
+import json
+import os
+import numpy as np
+from matplotlib import pyplot
+
+
+class StopTrainingCallback(keras.callbacks.Callback):
+    training_stopped = False
+
+    def __init__(self, stop_event):
+        super().__init__()
+        self.stop_event = stop_event
+
+    def on_batch_end(self, batch, logs=None):
+        if self.stop_event.is_set():
+            print("Inside StopTrainingCallback, stop was received")
+            self.model.stop_training = True
+            StopTrainingCallback.training_stopped = True
+
+
+class DigitModelTrainer:
+    NUM_CLASSES = 10
+    IMG_HEIGHT = 28
+    IMG_WIDTH = 28
+    CHANNEL_DIMENSION = 1
+    MODEL_FILE = "ai_digits_model.h5"
+
+    def __init__(self):
+        # Connect to Redis instance
+        self.redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.model = None
+
+    def load_model(self):
+        if os.path.isfile(self.MODEL_FILE):
+            self.model = keras.models.load_model(self.MODEL_FILE)
+        else:
+            print("Model file does not exist. Please create the model first.")
+
+
+    def save_epoch_details(self, epoch, logs=None):
+        if StopTrainingCallback.training_stopped:
+            print("Inside save_epoch details. gonna empty")
+            self.redis_conn.set('training_progress', '{}')
+            return
+
+        print("saving epoch details")
+        logs = logs or {}
+        updated_progress = {
+            'accuracy': logs.get('accuracy'),
+            'epoch': epoch + 1
+        }
+        self.redis_conn.set('training_progress', json.dumps(updated_progress))
+
+    def build_model(self, stop_training_event):
+        # Reset the flag in stopTrainingCallback
+        StopTrainingCallback.training_stopped = False  # Reset the flag
+        # Empty training_progress
+        self.redis_conn.set('training_progress', '{}')
+
+        # Load MNIST data
+        (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+
+        num_training_images = X_train.shape[0]
+        num_testing_images = X_test.shape[0]
+        # Reshape the data and add channel dimension
+        X_train = X_train.reshape(num_training_images, self.IMG_HEIGHT, self.IMG_WIDTH, self.CHANNEL_DIMENSION)
+        X_test= X_test.reshape(num_testing_images, self.IMG_HEIGHT, self.IMG_WIDTH, self.CHANNEL_DIMENSION)
+
+        # Normalize pixel values
+        X_train, X_test = X_train / 255.0, X_test / 255.0
+
+        # Create One-hot encode labels.
+        y_train = keras.utils.to_categorical(y_train, self.NUM_CLASSES)
+        y_test = keras.utils.to_categorical(y_test, self.NUM_CLASSES)
+
+        # Make model
+        # Create a convolutional neural network
+        self.model = keras.models.Sequential([
+            keras.layers.Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            keras.layers.Conv2D(64, kernel_size=(5, 5), activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            keras.layers.Flatten(),
+            keras.layers.Dense(128, activation='relu'),
+            keras.layers.Dropout(0.5),
+            keras.layers.Dense(10, activation='softmax')
+        ])
+
+        # Learning rate scheduling. The optimization algorithmn ("adam" in this case)
+        # will have a decayed learning rate the longer the training goes on
+        initial_learning_rate = 0.001
+        decay_steps = 400
+        decay_rate = 0.93
+
+        lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate,
+            decay_steps=decay_steps,
+            decay_rate=decay_rate,
+            staircase=True)
+
+        optimizer = keras.optimizers.Adam(learning_rate=lr_schedule)
+
+        # Train neural network
+        self.model.compile(
+            optimizer=optimizer,
+            loss="categorical_crossentropy",
+            metrics=["accuracy"]
+        )
+
+
+        # New instance of callback class, which will check the signal (always when a batch ends)
+        stop_training_callback = StopTrainingCallback(stop_training_event)
+
+        # Callback which will run 'save_epoch_detail' on each epoch end
+        epoch_callback = keras.callbacks.LambdaCallback(on_epoch_end=self.save_epoch_details)
+
+        self.model.fit(X_train, y_train, epochs=10, callbacks=[epoch_callback, stop_training_callback])
+
+        # Evaluate neural network performance
+        _, accuracy = self.model.evaluate(X_test,  y_test, verbose=2)
+        self.model.save(self.MODEL_FILE)
+
+        return self.model, accuracy
+    
+    # Analyze drawing and predict digit
+    # returns the index of the maximum value in the provided array, which has probabilities for each digit
+    def predict_digit(self, image_arr):
+        if self.model is None:
+            return (None, None)
+        input_data = image_arr.reshape(1, 28, 28, 1)  # Reshape for the model
+        prediction_array = self.model.predict(input_data)
+        predicted_digit = int(np.argmax(prediction_array))  # Get the index of the maximum value which is our digit and convert to native Python int
+        # Convert the max probability to percentage (of confidence)
+        # Assuming prediction[0] exists here is ok, keras returns (batch_size, num_classes) --> batch_size is 1, because we're analyzing 1 image
+        confidence_percentage = float(prediction_array[0][predicted_digit] * 100) 
+        print(f"Predicted digit: {predicted_digit}, confidence {confidence_percentage}%")
+        return predicted_digit, confidence_percentage
